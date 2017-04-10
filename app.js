2b9f18018d884e72f914a8dec9eac3a5e2082455
Adam Hammond <adam_hammond@uk.ibm.com>
2017-04-10 11:55:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ function ensureAuthenticated(req, res, next) {
 // handle callback, if authentication succeeds redirect to
 // original requested url, otherwise go to /failure
 app.get('/auth/callback', 
-//	passport.authenticate('openidconnect', { failureRedirect: '/failure' }),
+	passport.authenticate('openidconnect', { failureRedirect: '/failure' }),
 	function(req, res) {
 //    		res.redirect(req.session.originalUrl);
 		res.send('Redirect to: ' + req.session.originalUrl);

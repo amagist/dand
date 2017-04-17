63208b6dad7e7eb8b3bfbd9dfbfee9e5c70dcc30
Adam Hammond <adam_hammond@uk.ibm.com>
2017-04-17 11:34:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.get('/logout', function(req,res) {
 app.use(express.static(__dirname + '/public'));
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+app.listen(appEnv.port || 8080, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });

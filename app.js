2448e57c1c819c2a236957da2924e9978ed6a38f
THOMAS Sherlock <tsherloc@uk.ibm.com>
2018-05-08 16:33:44 +0100

    initial commit with working w3id code

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var _ = require("lodash");
 var path = require("path");
 var when = require("when");
@@ -16,6 +9,7 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var RED = require("node-red");
+const util = require("util")
 
 // read settings.js
 var OIDsettings = require('./oid-settings.js');
@@ -23,8 +17,6 @@ var OIDsettings = require('./oid-settings.js');
 // work around intermediate CA issue
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
 var VCAP_APPLICATION = JSON.parse(process.env.VCAP_APPLICATION);
@@ -35,39 +27,43 @@ var VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES);
 var app = express();
 
 app.use(cookieParser());
-app.use(session({resave: 'true', saveUninitialized: 'true' , secret: 'keyboard cat'}));
+app.use(session({
+  resave: 'true',
+  saveUninitialized: 'true',
+  secret: 'keyboard cat'
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser(function(user, done) {
-	   done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
-	   done(null, obj);
+  done(null, obj);
 });
 
 var OpenIDConnectStrategy = require('passport-idaas-openidconnect').IDaaSOIDCStrategy;
 var Strategy = new OpenIDConnectStrategy({
-                 authorizationURL : OIDsettings.authorization_url,
-                 tokenURL : OIDsettings.token_url,
-                 clientID : OIDsettings.client_id,
-                 scope: 'openid',
-                 response_type: 'code',
-                 clientSecret : OIDsettings.client_secret,
-                 callbackURL : OIDsettings.callback_url,
-                 skipUserProfile: true,
-                 issuer: OIDsettings.issuer_id,
-                 addCACert: true,
-		 CACertPathList: ['/oidc_w3id.cer']
-                 },
-         function(iss, sub, profile, accessToken, refreshToken, params, done)  {
-	        process.nextTick(function() {
-                profile.accessToken = accessToken;
-		profile.refreshToken = refreshToken;
-		done(null, profile);
-	      	})
-});
+    authorizationURL: OIDsettings.authorization_url,
+    tokenURL: OIDsettings.token_url,
+    clientID: OIDsettings.client_id,
+    scope: 'openid',
+    response_type: 'code',
+    clientSecret: OIDsettings.client_secret,
+    callbackURL: OIDsettings.callback_url,
+    skipUserProfile: true,
+    issuer: OIDsettings.issuer_id,
+    addCACert: true,
+    CACertPathList: ['/oidc_w3id.cer']
+  },
+  function(iss, sub, profile, accessToken, refreshToken, params, done) {
+    process.nextTick(function() {
+      profile.accessToken = accessToken;
+      profile.refreshToken = refreshToken;
+      done(null, profile);
+    })
+  });
 
 passport.use(Strategy);
 
@@ -79,73 +75,80 @@ app.get('/login', passport.authenticate('openidconnect', {}));
 
 // validate login
 function ensureAuthenticated(req, res, next) {
-	if (!req.isAuthenticated()) {
-		req.session.originalUrl = req.originalUrl;
-		res.redirect('/login');
-	} else {
-		return next();
-	}
+  // return next(); // remove this to remove authentication
+  if (!req.isAuthenticated()) {
+    req.session.originalUrl = req.originalUrl;
+    res.redirect('/login');
+  } else {
+    //  return next();
+    next();
+  }
 }
 
-// handle callback, if authentication succeeds redirect to
-// original requested url, otherwise go to /failure
-app.get('/auth/callback', function(req,res,next) {
-    var redirect_url = req.session.originalUrl;
-        passport.authenticate('openidconnect', {
-                successRedirect: redirect_url,
-                failureRedirect: '/failure',
-        })(req,res,next);
-    });
+// handle callback, if authentication succeeds redirect to original requested url, otherwise go to /failure
+app.get('/auth/callback', function(req, res, next) {
+  var redirect_url = req.session.originalUrl;
+  passport.authenticate('openidconnect', {
+    successRedirect: redirect_url,
+    failureRedirect: '/failure',
+  })(req, res, next);
+});
 
-app.get('/logout', function(req,res) {
-        req.session.destroy();
-        req.logout();
-        res.send("Logged out");
-     });
+// never used - to be removed?
+app.get('/logout', function(req, res) {
+  req.session.destroy();
+  req.logout();
+  res.send("Logged out");
+});
 
 
 var REDsettings = {
-    mqttReconnectTime: 15000,
-    serialReconnectTime: 15000,
-    debugMaxLength: 1000,
+  mqttReconnectTime: 15000,
+  serialReconnectTime: 15000,
+  debugMaxLength: 1000,
 
-    // Add the bluemix-specific nodes in
-    nodesDir: path.join(__dirname,"nodes"),
+  // Add the bluemix-specific nodes in
+  nodesDir: path.join(__dirname, "nodes"),
 
-    // Blacklist the non-bluemix friendly nodes
-    nodesExcludes:['66-mongodb.js','75-exec.js','35-arduino.js','36-rpi-gpio.js','25-serial.js','28-tail.js','50-file.js','31-tcpin.js','32-udp.js','23-watch.js'],
+  // Blacklist the non-bluemix friendly nodes
+  nodesExcludes: ['66-mongodb.js', '75-exec.js', '35-arduino.js', '36-rpi-gpio.js', '25-serial.js', '28-tail.js', '50-file.js', '31-tcpin.js', '32-udp.js', '23-watch.js'],
 
-    // Enable module reinstalls on start-up; this ensures modules installed
-    // post-deploy are restored after a restage
-    autoInstallModules: true,
+  // Enable module reinstalls on start-up; this ensures modules installed are restored after a restage
+  autoInstallModules: true,
 
-    // paths
-    httpAdminRoot: '/red',
-    httpNodeRoot: '/',
+  // paths
+  httpAdminRoot: '/red',
+  httpNodeRoot: '/',
 
-    // UI
-    ui: { path: "ui" },
+  // UI
+  ui: {
+    path: "ui"
+  },
 
-    functionGlobalContext: { },
+  functionGlobalContext: {},
 
-    storageModule: require("./couchstorage")
+  storageModule: require("./couchstorage"),
+  adminAuth: {
+    type: "credentials",
+    users: [{
+      username: "admin",
+      password: "$2a$08$RWESklWuO.QnyaHyoszxWuZda21OAuu3KUyBrKvih9zhk23ozwl9i", // paste your password hash here
+      permissions: "*"
+    }]
+  }
+
 };
 
 REDsettings.couchAppname = VCAP_APPLICATION['application_name'];
-console.log("App name: " + REDsettings.couchAppname);
-console.log("Test: " + "^"+REDsettings.couchAppname+".cloudantNoSQLDB");
-var storageServiceName = process.env.NODE_RED_STORAGE_NAME || new RegExp("^"+REDsettings.couchAppname+".Cloudant");
-
-
-console.log("Storage service name: " + storageServiceName);
+var storageServiceName = process.env.NODE_RED_STORAGE_NAME || new RegExp("^" + REDsettings.couchAppname + ".Cloudant");
 var couchService = appEnv.getService(storageServiceName);
 
 if (!couchService) {
-    console.log("Failed to find Cloudant service");
-    if (process.env.NODE_RED_STORAGE_NAME) {
-        console.log(" - using NODE_RED_STORAGE_NAME environment variable: "+process.env.NODE_RED_STORAGE_NAME);
-    }
-    throw new Error("No cloudant service found");
+  console.log("Failed to find Cloudant service");
+  if (process.env.NODE_RED_STORAGE_NAME) {
+    console.log(" - using NODE_RED_STORAGE_NAME environment variable: " + process.env.NODE_RED_STORAGE_NAME);
+  }
+  throw new Error("No cloudant service found");
 }
 REDsettings.couchUrl = couchService.credentials.url;
 
@@ -153,7 +156,7 @@ REDsettings.couchUrl = couchService.credentials.url;
 var server = http.createServer(app);
 
 // Initialise the runtime with a server and settings
-RED.init(server,REDsettings);
+RED.init(server, REDsettings);
 
 // Serve the editor UI - must be authenticated
 app.use(REDsettings.httpAdminRoot, ensureAuthenticated, RED.httpAdmin);
@@ -162,19 +165,24 @@ app.use(REDsettings.httpAdminRoot, ensureAuthenticated, RED.httpAdmin);
 app.use(REDsettings.httpNodeRoot, RED.httpNode);
 
 // Serve the http nodes UI (uncomment following line if you require authenticated endpoint - needed for authenticated dashboard)
-// app.use(REDsettings.httpNodeRoot, ensureAuthenticated, RED.httpNode);
+app.use(REDsettings.httpNodeRoot, ensureAuthenticated, RED.httpNode);
 
 // failure page (can be overriden within NodeRED)
 app.get('/failure', function(req, res) {
-	res.send('Login failed'); });
+  res.send('Login failed');
+});
 
 // Check authentication page (can be overriden within NodeRED)
 app.get('/hello', ensureAuthenticated, function(req, res) {
-	res.send('Hello, '+ req.user['id'] + '!');
-        });
+  res.send('Hello, ' + req.user['id'] + '!');
+});
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+// serve the files out of ./public as our main files (regex to catch all)
+app.get(/^(.+)$/, ensureAuthenticated, function(req, res) {
+  res.sendfile(__dirname + "/public/" + req.params[0]);
+});
+
+app.use(express.static(__dirname + '/public'), ensureAuthenticated);
 
 // start server on the specified port and binding host
 server.listen(appEnv.port);
--- oid-settings.js
+++ oid-settings.js
@@ -1,6 +1,7 @@
-exports.client_id = "YTU2MGZkZWUtODM0Yy00";
-exports.client_secret = "MDAyMTNmMDUtNTFiZi00";
+// grab these from the SSO registration page, paste them in here
+exports.client_id = "YWJjY2Y3YjctN2RjNy00";
+exports.client_secret = "M2Y5YTc5MTgtYjk4MS00";
 exports.authorization_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
 exports.token_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
 exports.issuer_id = "https://w3id.alpha.sso.ibm.com/isam";
-exports.callback_url = "https://gdpr-chat.w3ibm.mybluemix.net/auth/callback";
+exports.callback_url = "https://RPT-microsite-test.w3ibm.mybluemix.net/auth/callback";

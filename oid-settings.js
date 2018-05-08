ee3e01002a201a0f04499c943b4def6e73e19923
THOMAS Sherlock <tsherloc@uk.ibm.com>
2018-05-08 16:50:08 +0100

    fixed

--- oid-settings.js
+++ oid-settings.js
@@ -4,4 +4,4 @@ exports.client_secret = "M2Y5YTc5MTgtYjk4MS00";
 exports.authorization_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
 exports.token_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
 exports.issuer_id = "https://w3id.alpha.sso.ibm.com/isam";
-exports.callback_url = "https://RPT-microsite-test.w3ibm.mybluemix.net/auth/callback";
+exports.callback_url = "https://ibm-rapid-prototyping.w3ibm.mybluemix.net/auth/callback";

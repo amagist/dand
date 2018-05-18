84ac9b6154d49f80735649101a9415a093831eb9
THOMAS Sherlock <tsherloc@uk.ibm.com>
2018-05-18 16:30:55 +0100

    touching up yeoman configs

--- generator-deploy/generators/app/index.js
+++ generator-deploy/generators/app/index.js
@@ -1,13 +1,6 @@
 var Generator = require('yeoman-generator');
 module.exports = class extends Generator {
-  writing() {
-      this.fs.copyTpl(
-        this.templatePath('index.html'),
-        this.destinationPath('./../public/index.html'),
-        { title: 'Templating with Yeoman' }
-      );
-    }
-/*  prompting() {
+  prompting() {
     this.log("\n Auto-config for microsites. First, take a look at the readme.")
     this.log("\n If using authentication, register your SSO app at https://w3.innovate.ibm.com/tools/sso/home.html, and grab your client ID and secret. You'll have to copy in the certificate yourself")
     this.log('\n Note: you can only run an authenticated app on IBM CIO Cloud (w3ibm.mybluemix.net), not on public (at the moment)')
@@ -24,7 +17,17 @@ module.exports = class extends Generator {
         type: 'confirm',
         name: 'authentication',
         message: 'Will you be requiring authenticated endpoints?'
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: '(After creating an SSO config) What is your Client ID? (leave blank if not authenticating)'
       }, {
+        type: 'input',
+        name: 'secret',
+        message: '(After creating an SSO config) What is your Client secret? (leave blank if not authenticating)'
+      },
+      {
         type: 'input',
         name: 'username',
         message: 'Enter a username for your node-RED editor',
@@ -41,14 +44,53 @@ module.exports = class extends Generator {
       if (!answers.dedicated && answers.authentication) {
         this.log("This isn't possible. I'll add some proper error handling in here at some point, but until then, restart the script - either it needs to run on w3 Bluemix with authentication, or on public with no authentication")
       } else {
-        this.log('app name: ', answers.name);
-        this.log('dedicated: ', answers.dedicated);
-        this.log('username: ', answers.username);
-        this.log('password: ', answers.password);
-        this.log(require('bcryptjs').hashSync(answers.password, 8))
+        if (answers.dedicated) {
+          this.config.set("domain", "w3ibm.mybluemix.net")
+          this.config.set("DB", (answers.name + "-Cloudant NoSQL DB Dedicated"))
+        } else {
+          this.config.set("domain", "eu-gb.mybluemix.net")
+          this.config.set("DB", (answers.name + "-Cloudant NoSQL DB"))
+        }
+        this.config.save()
+        this.config.set("name", answers.name)
+        this.config.set("dedicated", answers.dedicated)
+        this.config.set("username", answers.username)
+        this.config.set("password", require('bcryptjs').hashSync(answers.password, 8))
+        this.config.set("id", answers.id)
+        this.config.set("secret", answers.secret)
+        this.config.set("authentication", answers.authentication)
       }
-
     });
   }
-*/
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('manifest.yml'),
+      this.destinationPath('./manifest.yml'), {
+        domain: this.config.get('domain'),
+        name: this.config.get('name'),
+        service: this.config.get('DB')
+      }
+    ); // done creating manifest.yml
+    this.fs.copyTpl(
+      this.templatePath('oid-settings.js'),
+      this.destinationPath('./oid-settings.js'), {
+        name: this.config.get('name'),
+        id: this.config.get('id'),
+        secret: this.config.get('secret')
+      }
+    ); // done creating OIDC settings
+    this.fs.copyTpl(
+      this.templatePath('redAuth.js'),
+      this.destinationPath('./redAuth.js'), {
+        username: this.config.get('username'),
+        password: this.config.get('password'),
+      }
+    ); // done creating red auth file
+    this.fs.copyTpl(
+      this.templatePath('auth.js'),
+      this.destinationPath('./auth.js'), {
+        authentication: this.config.get('authentication'),
+      }
+    ); // done creating auth file
+  }
 };
--- generator-deploy/generators/app/templates/oid-settings.js
+++ generator-deploy/generators/app/templates/oid-settings.js
@@ -1,7 +1,7 @@
 // grab these from the SSO registration page, paste them in here
-exports.client_id = "<client_id>";
-exports.client_secret = "<client_secret>";
+exports.client_id = "<%= id %>";
+exports.client_secret = "<%= secret %>";
 exports.authorization_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
 exports.token_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
 exports.issuer_id = "https://w3id.alpha.sso.ibm.com/isam";
-exports.callback_url = "https://<appname>.w3ibm.mybluemix.net/auth/callback";
+exports.callback_url = "https://<%= name %>.w3ibm.mybluemix.net/auth/callback";

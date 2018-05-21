fabc4f1079d9372a34d74324a716807a42e3d101
THOMAS Sherlock <tsherloc@uk.ibm.com>
2018-05-21 10:48:27 +0100

    VScode config

--- generator-deploy/generators/app/index.js
+++ generator-deploy/generators/app/index.js
@@ -5,40 +5,40 @@ module.exports = class extends Generator {
     this.log("\n If using authentication, register your SSO app at https://w3.innovate.ibm.com/tools/sso/home.html, and grab your client ID and secret. You'll have to copy in the certificate yourself")
     this.log('\n Note: you can only run an authenticated app on IBM CIO Cloud (w3ibm.mybluemix.net), not on public (at the moment)')
     return this.prompt([{
-        type: 'input',
-        name: 'name',
-        message: 'Enter the name of your app',
-        default: 'sample-microsite'
-      }, {
-        type: 'confirm',
-        name: 'dedicated',
-        message: 'Will you be running on IBM CIO (private) cloud?'
-      }, {
-        type: 'confirm',
-        name: 'authentication',
-        message: 'Will you be requiring authenticated endpoints?'
-      },
-      {
-        type: 'input',
-        name: 'id',
-        message: '(After creating an SSO config) What is your Client ID? (leave blank if not authenticating)'
-      }, {
-        type: 'input',
-        name: 'secret',
-        message: '(After creating an SSO config) What is your Client secret? (leave blank if not authenticating)'
-      },
-      {
-        type: 'input',
-        name: 'username',
-        message: 'Enter a username for your node-RED editor',
-        default: 'admin'
-      },
-      {
-        type: 'input',
-        name: 'password',
-        message: 'Enter a password for your node-RED editor',
-        default: 'password'
-      }
+      type: 'input',
+      name: 'name',
+      message: 'Enter the name of your app',
+      default: 'sample-microsite'
+    }, {
+      type: 'confirm',
+      name: 'dedicated',
+      message: 'Will you be running on IBM CIO (private) cloud?'
+    }, {
+      type: 'confirm',
+      name: 'authentication',
+      message: 'Will you be requiring authenticated endpoints?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: '(After creating an SSO config) What is your Client ID? (leave blank if not authenticating)'
+    }, {
+      type: 'input',
+      name: 'secret',
+      message: '(After creating an SSO config) What is your Client secret? (leave blank if not authenticating)'
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Enter a username for your node-RED editor',
+      default: 'admin'
+    },
+    {
+      type: 'input',
+      name: 'password',
+      message: 'Enter a password for your node-RED editor',
+      default: 'password'
+    }
 
     ]).then((answers) => {
       if (!answers.dedicated && answers.authentication) {
